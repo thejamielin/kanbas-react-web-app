5d008d8603fb518a87ad9da6cea41c80691a0f9d
Jamie Lin <jamie.miki.lin@gmail.com>
2024-02-07 20:56:52 -0500

    complete working with arrays portion of lab

--- src/Labs/a3/JavaScript/WorkingWithArrays.tsx
+++ src/Labs/a3/JavaScript/WorkingWithArrays.tsx
@@ -1,12 +1,26 @@
 import Arrays from "./arrays/Arrays";
 import ArrayIndexAndLength from "./arrays/ArrayIndexAndLength";
+import AddingAndRemovingDataToFromArrays from "./arrays/AddingAndRemovingDataToFromArrays";
+import ForLoops from "./arrays/ForLoops";
+import MapFunction from "./arrays/MapFunction";
+import JsonStringify from "./json/JsonStringify";
+import FindFunction from "./arrays/FindFunction";
+import FindIndex from "./arrays/FindIndex";
+import FilterFunction from "./arrays/FilterFunction";
 
 function WorkingWithArrays() {
   return (
     <>
-      <h1>Working With Functions</h1>
+      <h1>Working With Arrays</h1>
       <Arrays />
       <ArrayIndexAndLength />
+      <AddingAndRemovingDataToFromArrays />
+      <ForLoops />
+      <MapFunction />
+      <JsonStringify />
+      <FindFunction />
+      <FindIndex />
+      <FilterFunction />
     </>
   );
 }
