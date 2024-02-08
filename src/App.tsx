9197aa3a656cb52f9fddbd417470953364d16131
Jamie Lin <jamie.miki.lin@gmail.com>
2024-02-07 19:55:44 -0500

    work through a3's lab, complete up to 2.2.6 working with functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Labs from "./Labs";
 import HelloWorld from "./Labs/a3/HelloWorld";
+import Kanbas from "./Kanbas";
 
 function App() {
   return (
     <div>
       <Labs />
+      <Kanbas />
       <HelloWorld />
     </div>
   );
---
+++ src/Labs/a3/JavaScript/WorkingWithFunctions/index.tsx
@@ -0,0 +1,12 @@
+import ES5Functions from "../functions/ES5Functions";
+
+function WorkingWithFunctions() {
+  return (
+    <>
+      <h1>Working With Functions</h1>
+      <ES5Functions />
+    </>
+  );
+}
+
+export default WorkingWithFunctions;
