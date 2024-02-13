270e776542f29d6f0b33264e916aa75b9ab12fc7
Jamie Lin <jamie.miki.lin@gmail.com>
2024-02-13 15:10:38 -0500

    download react router and react router dom and add navigation links

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,8 +1,11 @@
+import { Link } from "react-router-dom";
 import Assignment3 from "./a3";
 
 function Labs() {
   return (
     <div>
+      <Link to="/Labs/a3">A3</Link>|<Link to="/Kanbas">Kanbas</Link>|
+      <Link to="/hello">Hello</Link>
       <Assignment3 />
     </div>
   );
