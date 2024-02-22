371df75fa07bb672783508c1e6d3419e5ee533e0
Jamie Lin <jamie.miki.lin@gmail.com>
2024-02-21 19:12:14 -0500

    finish 2.2 of lab

--- src/Labs/a4/index.tsx
+++ src/Labs/a4/index.tsx
@@ -1,9 +1,21 @@
 import React from "react";
+import ClickEvent from "./ClickEvent";
+import PassingDataOnEvent from "./PassingDataOnEvent";
+import PassingFunctions from "./PassingFunctions";
+import EventObject from "./EventObject";
 
 const Assignment4 = () => {
+  function sayHello() {
+    alert("Hello");
+  }
+
   return (
     <>
       <h1>Assignment 4</h1>
+      <ClickEvent />
+      <PassingDataOnEvent />
+      <PassingFunctions theFunction={sayHello} />
+      <EventObject />
     </>
   );
 };
