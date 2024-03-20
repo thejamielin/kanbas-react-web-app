cf0cdc64298635609bfcbb5690e8cb54ee04cd42
Jamie Lin <jamie.miki.lin@gmail.com>
2024-03-20 18:55:28 -0400

    complete 3.1.3 on my own section

--- src/Labs/a5/WorkingWithObjects.tsx
+++ src/Labs/a5/WorkingWithObjects.tsx
@@ -8,7 +8,14 @@ function WorkingWithObjects() {
     completed: false,
     score: 0,
   });
+  const [module, setModule] = useState({
+    id: 1,
+    name: "NodeJS Module",
+    description: "Info on how to create a NodeJS server with ExpressJS",
+    course: "Web Development",
+  });
   const ASSIGNMENT_URL = "http://localhost:4000/a5/assignment";
+  const MODULE_URL = "http://localhost:4000/a5/module";
   return (
     <div>
       <h3>Working With Objects</h3>
@@ -36,6 +43,54 @@ function WorkingWithObjects() {
         href="http://localhost:4000/a5/assignment/title"
       >
         Get Title
+      </a>
+      <h3>Working With Objects - On My Own</h3>
+      <h4>Retrieving Objects</h4>
+      <a className="btn btn-primary" href="http://localhost:4000/a5/module">
+        Get Module
+      </a>
+      <h4>Retrieving Properties</h4>
+      <a
+        className="btn btn-primary"
+        href="http://localhost:4000/a5/module/name"
+      >
+        Get Module Name
+      </a>
+      <h4>Modifying Properties</h4>
+      <input
+        type="text"
+        onChange={(e) => setModule({ ...module, name: e.target.value })}
+        value={module.name}
+      />
+      <a className="btn btn-primary" href={`${MODULE_URL}/name/${module.name}`}>
+        Update Module Name
+      </a>
+      <h4>Modifying More Assignment Properties</h4>
+      <input
+        type="number"
+        onChange={(e) =>
+          setAssignment({ ...assignment, score: Number(e.target.value) })
+        }
+        value={assignment.score}
+      />
+      <a
+        className="btn btn-primary"
+        href={`${ASSIGNMENT_URL}/score/${assignment.score}`}
+      >
+        Update Score
+      </a>
+      <input
+        type="checkbox"
+        onChange={(e) =>
+          setAssignment({ ...assignment, completed: e.target.checked })
+        }
+        value={assignment.score}
+      />
+      <a
+        className="btn btn-primary"
+        href={`${ASSIGNMENT_URL}/completed/${assignment.completed}`}
+      >
+        Update Completed Status
       </a>
     </div>
   );
